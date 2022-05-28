12d3c9bd126a5337b83d997f3971332b58d3c739
OldStudioConcept <58786711+OldStudioConcept@users.noreply.github.com>
2022-05-29 00:28:52 +0300

    t

--- js/main.js
+++ js/main.js
@@ -1,74 +1,4 @@
-let clicable = true;
-$('.section-5 .card').on('click', function(){
-    if(clicable == false){
-        return false;
-    }
-    let card = $(this).data('card')
-    let title = $(this).find('p').text();
-    $('.section-5 .content .number').text('0' + card)
-    $('.section-5 .content h2').text(title)
 
-    $('.section-5 .content').show();
-    $('.section-5 .content').animate({
-            width:'100%',
-            padding: '20px 50px',
-            height:'100%'
-        }, function(){
-            $('.section-5 .content .close').fadeIn();
-            $('.section-5 .content .icon').fadeIn();
-            $('.section-5 .content .number').fadeIn();
-            $('.section-5 .content h2').fadeIn();
-            $('.section-5 .content p').fadeIn();
-            $('.section-5 .content').addClass('visible');
-        });
-})
-
-$('.section-5 .content .close').on("click", function(){
-    clicable = false;
-    $('.section-5 .content .close').fadeOut();
-    $('.section-5 .content .number').fadeOut();
-    $('.section-5 .content .icon').fadeOut();
-    $('.section-5 .content h2').fadeOut();
-    $('.section-5 .content').removeClass('visible');
-    $('.section-5 .content p').fadeOut(function(){
-        $('.section-5 .content').animate({
-            width:'0%',
-            padding:'0%',
-            height:'0%'
-        }, function(){
-            $('.section-5 .content').hide();
-        });
-    });
-    setTimeout(() => {
-        clicable = true;
-    }, 1500);
-})
-
-$(document).keyup(function(e) {
-    if (e.key === "Escape") { 
-        if(!$('.section-5 .content').hasClass('visible')){
-            return false;
-        }
-        clicable = false;
-        $('.section-5 .content .close').fadeOut();
-        $('.section-5 .content .number').fadeOut();
-        $('.section-5 .content').removeClass('visible');
-        $('.section-5 .content .icon').fadeOut();
-        $('.section-5 .content h2').fadeOut();
-        $('.section-5 .content p').fadeOut(function(){
-            $('.section-5 .content').animate({
-                width:'0%',
-                padding:'0%',
-                height:'0%'
-            }, function(){
-                $('.section-5 .content').hide();
-            });
-        });
-        setTimeout(() => {
-            clicable = true;
-        }, 1500);
-    }
-});
 
 let section9Content = [
     {
@@ -227,3 +157,79 @@ $('.close').on("click", function(){
     });
 
 })
+
+
+let clicable = true;
+$('.section-5 .card').on('click', function(){
+    if(clicable == false){
+        return false;
+    }
+    let card = $(this).data('card')
+    let title = $(this).find('p').text();
+    console.log(sectionSlide4Content[card]['text'])
+    $('.section-5 .content .number').text('0' + card)
+    $('.section-5 .content h2').text(title)
+    $('.section-5 .content').show();
+    $('.section-5 .content').animate({
+            width:'100%',
+            padding: '20px 50px',
+            height:'100%'
+        }, function(){
+            $('.section-5 .content .close').fadeIn();
+            $('.section-5 .content .icon').fadeIn();
+            $('.section-5 .content .number').fadeIn();
+            $('.section-5 .content h2').fadeIn();
+            $('.section-5 .content .text').fadeIn();
+            $('.section-5 .content .text').html(sectionSlide4Content[card]['text']);
+            $('.section-5 .content .text p').fadeIn();
+            $('.section-5 .content').addClass('visible');
+
+        });
+})
+
+$('.section-5 .content .close').on("click", function(){
+    clicable = false;
+    $('.section-5 .content .close').fadeOut();
+    $('.section-5 .content .number').fadeOut();
+    $('.section-5 .content .icon').fadeOut();
+    $('.section-5 .content h2').fadeOut();
+    $('.section-5 .content').removeClass('visible');
+    $('.section-5 .content p').fadeOut(function(){
+        $('.section-5 .content').animate({
+            width:'0%',
+            padding:'0%',
+            height:'0%'
+        }, function(){
+            $('.section-5 .content').hide();
+        });
+    });
+    setTimeout(() => {
+        clicable = true;
+    }, 1500);
+})
+
+$(document).keyup(function(e) {
+    if (e.key === "Escape") { 
+        if(!$('.section-5 .content').hasClass('visible')){
+            return false;
+        }
+        clicable = false;
+        $('.section-5 .content .close').fadeOut();
+        $('.section-5 .content .number').fadeOut();
+        $('.section-5 .content').removeClass('visible');
+        $('.section-5 .content .icon').fadeOut();
+        $('.section-5 .content h2').fadeOut();
+        $('.section-5 .content p').fadeOut(function(){
+            $('.section-5 .content').animate({
+                width:'0%',
+                padding:'0%',
+                height:'0%'
+            }, function(){
+                $('.section-5 .content').hide();
+            });
+        });
+        setTimeout(() => {
+            clicable = true;
+        }, 1500);
+    }
+});
